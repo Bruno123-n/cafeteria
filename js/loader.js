dcd2639ca91108441a18f79775ae48756b7ebbc1
Bruno123-n <hungriakaa@gmail.com>
2026-05-18 16:07:35 -0300

    git add. git commit -m "feat: add animated mobile menu and overlay"

---
+++ js/loader.js
@@ -0,0 +1,11 @@
+const loader =
+document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+
+        loader.classList.add(
+            'hidden'
+        );
+
+    }
+);
--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,30 @@
-const menuMobile =
-document.querySelector('.menu-mobile');
+const menuToggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+const navLinks = document.querySelectorAll("nav a");
 
-const nav =
-document.querySelector('nav');
 
-menuMobile.addEventListener('click', () => {
+menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.add("no-scroll")
+});
 
-        nav.classList.toggle('active');
+overlay.addEventListener("click", () => {
+    menuToggle.classList.remove("active");
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("no-scroll")
+});
 
-    }
-);
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        menuToggle.classList.remove("active");
+        menu.classList.remove("active");
+        document.body.classList.remove("no-scroll")
+  });
+});
+
+
